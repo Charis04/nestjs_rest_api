aca3a12375292a2f791018424bf4744170628485
charis <charisadu@gmail.com>
2025-06-17 02:45:06 +0100

    feat: add employee management with Prisma integration

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '../../generated/prisma';
+import { DatabaseService } from '../database/database.service';
+
+@Injectable()
+export class EmployeesService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  /**
+   * Creates a new employee.
+   * @param createEmployeeDto - The data to create a new employee.
+   * @returns A message indicating the action.
+   */
+  async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.databaseService.employee.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) return this.databaseService.employee.findMany({
+      where: {
+        role,
+      },
+    });
+    return this.databaseService.employee.findMany();
+  }
+
+  async findOne(id: number) {
+    return `This action returns a #${id} employee`;
+  }
+
+  async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    return `This action updates a #${id} employee`;
+  }
+
+  async remove(id: number) {
+    return `This action removes a #${id} employee`;
+  }
+}
